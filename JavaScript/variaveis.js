6ce2b07b4adb32955483cfd033cae4de44de5429
José Wagner Dutra Porto <jwagnerdporto@gmail.com>
2023-07-24 11:29:42 -0300

    teste

---
+++ JavaScript/variaveis.js
@@ -0,0 +1,27 @@
+// VARIÁVEIS
+
+let idade = 26
+
+console.log(idade)
+
+// Tipos de variáveis
+
+idade = 30               // Number
+let numero = 30          // Number
+let altura = 1.77        // Number
+let = nome = 'Walisson'  // String(texto)
+let estudando = true     // Booleano (true ou false)
+
+console.log(numero, typeof numero) // typeof serve para imprimir no codigo o tipo da variavel
+console.log(altura, typeof altura)
+console.log(nome, typeof nome)
+console.log(estudando, typeof estudando)
+
+let outra
+
+console.log(outra)
+
+let nula = null
+
+console.log(nula)
+
