b0ef8a0a9d27944ec83d74c43cb1470a3e3698e9
José Wagner Dutra Porto <jwagnerdporto@gmail.com>
2023-07-25 20:25:26 -0300

    Aula 6

---
+++ JavaScript/condicionais.js
@@ -0,0 +1,29 @@
+// > ESTRUTURAS CONDICIONAIS
+
+const idade = 10
+
+if (idade >= 18) {
+    console.log('Você é maior de idade!')
+} else {
+    console.log('Você é menor de idade!')
+}
+
+// Se media >= 7, Aprovado
+// Se media < 7 e media >= 5, Recuperação
+// Se media < 5, Reprovado
+
+console.clear()
+
+let media = 5.5
+
+if (media >= 7) {
+    console.log('Aprovada(o)')
+} else if (media >= 5) {
+    console.log('Recuperação')
+} else {
+    console.log('Reprovada(o)')
+}
+
+
+
+
