b8e06df034f79314f5f506ddd361c88c47f1dcf4
José Wagner Dutra Porto <jwagnerdporto@gmail.com>
2023-07-31 16:43:42 -0300

    Aulas 5 e 6

--- Java/src/Animais/Cachorro.java
+++ Java/src/Animais/Cachorro.java
@@ -1,14 +1,86 @@
 package Animais;
 
 public class Cachorro {
-
-	public String nome;
-	public String cor;
-	public int altura;
-	public double peso;
-	public int tamanhoDoRabo;
-	public String estadoDeEspirito;
 	
+	//Atributos
+
+	private String nome;
+	private String cor;
+	private int altura;
+	private double peso;
+	private int tamanhoDoRabo;
+	private String estadoDeEspirito;
+	
+	//Construtores
+	
+	public Cachorro() {}
+
+	public Cachorro(String nome, String cor, int altura, double peso, int tamanhoDoRabo, String estadoDeEspirito) {
+		super();
+		this.nome = nome;
+		this.cor = cor;
+		this.altura = altura;
+		this.peso = peso;
+		this.tamanhoDoRabo = tamanhoDoRabo;
+		this.estadoDeEspirito = estadoDeEspirito;
+	}
+	
+	//Metodos
+	
+	public String getNome(String string) {
+		return this.nome;
+	
+	}
+
+	public void setNome(String nome) {
+		if (nome.equals("Lily")) {
+			this.nome = null;
+		}
+		this.nome = nome;
+	}
+	
+	
+	
+	public String getCor(String string) {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public int getAltura(int i) {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+	public double getPeso(double d) {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public int getTamanhoDoRabo() {
+		return tamanhoDoRabo;
+	}
+
+	public void setTamanhoDoRabo(int tamanhoDoRabo) {
+		this.tamanhoDoRabo = tamanhoDoRabo;
+	}
+
+	public String getEstadoDeEspirito(int i) {
+		return estadoDeEspirito;
+	}
+
+	public void setEstadoDeEspirito(String estadoDeEspirito) {
+		this.estadoDeEspirito = estadoDeEspirito;
+	}
+
 	public void comer(){}
 	
 	public void latir(){
@@ -20,13 +92,25 @@ public class Cachorro {
 	}
 	
 	public String interagir(String acao) {
-		if (acao.equals("carinho")) {
-			this.estadoDeEspirito = "feliz";
-		} else if (acao.equals("vai dormir")){
-			this.estadoDeEspirito = "bravo";
-		} else {
-			this.estadoDeEspirito = "neutro";
+		
+		switch (acao) {
+			case "carinho": this.estadoDeEspirito = "feliz"; break;
+			case "vai dormir": this.estadoDeEspirito = "bravo"; break;
+			case "pisar ba patinha": this.estadoDeEspirito = "triste"; break;
+			default: this.estadoDeEspirito = "neutro"; break;
 		}
+		
 		return estadoDeEspirito;
+		
+		//if (acao.equals("carinho")) {
+		//	this.estadoDeEspirito = "feliz";
+		//} else if (acao.equals("vai dormir")){
+		//	this.estadoDeEspirito = "bravo";
+		//} else {
+		//	this.estadoDeEspirito = "neutro";
+		//}
+		//return estadoDeEspirito;
+	
+		
 	}
 }
--- Java/src/Aula3.java
+++ Java/src/Aula3.java
@@ -5,12 +5,22 @@ public class Aula3 {
 	public static void main(String[] args) {
 		
 		Cachorro cachorro1 = new Cachorro();
+		Cachorro cachorro2 = new Cachorro("Puppy","Marrow", 25, 5.5,5, "nada");
 		
-		cachorro1.nome = "Puppy";
-		cachorro1.cor = "Marron";
-		cachorro1.altura = 25;
-		cachorro1.peso = 5.5;
-		cachorro1.tamanhoDoRabo = 5;
+		System.out.println(cachorro2.getNome(null));
+		System.out.println(cachorro2.getPeso(0));
+		
+//		cachorro1.getNome("Puppy");
+//		cachorro1.getCor("Marron");
+//		cachorro1.getAltura(25);
+//		cachorro1.getPeso(5.5);
+//		cachorro1.getEstadoDeEspirito(5);
+		
+//		cachorro1.nome = "Puppy";
+//		cachorro1.cor = "Marron";
+//		cachorro1.altura = 25;
+//		cachorro1.peso = 5.5;
+//		cachorro1.tamanhoDoRabo = 5;
 		
 		//cachorro1.latir();
 		//System.out.println("O cachorro pegou uma " + cachorro1.pegar());
